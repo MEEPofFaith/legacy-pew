a4090dca6bf26c191d4df1dd85b4b3193a3f2717
MEEP of Faith <meepmatthew@gmail.com>
2021-02-03 13:24:02 -0800

    Done

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# legacy-pew
+# Legacy Pew
 All vanilla units that use `Sounds.pew` will have their `shootSound` replaced with v5 pew.
---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+if(!Vars.headless){
+  Vars.content.units().each(u => {
+    if(u.minfo.mod === null){
+      u.weapons.each(w => {
+        w.shootSound = Sounds.pew_;
+      });
+    }
+  });
+  
+  Core.app.post(() => {
+    const meta = Vars.mods.locateMod("legacy-pew").meta;
+    meta.author = "[#FCC21B]MEEP of Faith";
+  });
+}
