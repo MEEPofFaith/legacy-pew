0b8f9e2c3228583fc51667b4bb91d3e648f55169
MEEP of Faith <meepmatthew@gmail.com>
2021-02-11 23:52:01 -0800

    Include laserShoot (Nova/Mega)

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,16 @@
 if(!Vars.headless){
-  Vars.content.units().each(u => {
-    if(u.minfo.mod === null){
-      u.weapons.each(w => {
-        if(w.shootSound == Sounds.pew) w.shootSound = Sounds.pew_;
+  Events.on(ClientLoadEvent, () => {
+    Core.app.post(() => {
+      Vars.content.units().each(u => {
+        if(u.minfo.mod === null){
+          u.weapons.each(w => {
+            if(w.shootSound == Sounds.pew || w.shootSound == Sounds.lasershoot) w.shootSound = Sounds.pew_;
+          });
+        }
       });
-    }
-  });
-  
-  Core.app.post(() => {
-    const meta = Vars.mods.locateMod("legacy-pew").meta;
-    meta.author = "[#FCC21B]MEEP of Faith";
+    
+      const meta = Vars.mods.locateMod("legacy-pew").meta;
+      meta.author = "[#FCC21B]MEEP of Faith";
+    });
   });
 }
